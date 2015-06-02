7c89f6919bf8172658ba56d5e6ddf9fc20150f91
Everett Toews <everett.toews@rackspace.com>
2015-06-01 20:44:32 -0500

    Deploy API fixes

--- code/api/deploy.sh
+++ code/api/deploy.sh
@@ -7,9 +7,9 @@ echo "source $HOME/api_env.rc" >> $HOME/.profile
 echo "source $HOME/db_env.rc" >> $HOME/.profile
 source $HOME/.profile
 
-git checkout https://github.com/$GITHUB_USERNAME/app-on-openstack.git
+git clone https://github.com/$GITHUB_USERNAME/app-on-openstack.git
 pip install virtualenv
 virtualenv api-venv
 source api-venv/bin/activate
 pip install -r $HOME/app-on-openstack/code/api/requirements.txt
-python $HOME/app-on-openstack/code/api/watermark/api.py runserver
+python $HOME/app-on-openstack/code/api/watermark/api.py runserver --host 0.0.0.0 &
