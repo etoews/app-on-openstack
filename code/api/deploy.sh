066f94a072abb3f3bd9ce86270f1b74ddae635ac
Everett Toews <everett.toews@rackspace.com>
2015-06-03 15:53:11 -0500

    More absolute paths

--- code/api/deploy.sh
+++ code/api/deploy.sh
@@ -9,7 +9,7 @@ source /root/.profile
 
 git clone https://github.com/$GITHUB_USERNAME/app-on-openstack.git /root/app-on-openstack
 pip install virtualenv
-virtualenv api-venv
-source api-venv/bin/activate
+virtualenv /root/api-venv
+source /root/api-venv/bin/activate
 pip install -r /root/app-on-openstack/code/api/requirements.txt
 python /root/app-on-openstack/code/api/manage.py runserver --host 0.0.0.0 &
--- code/app/deploy.sh
+++ code/app/deploy.sh
@@ -8,7 +8,7 @@ source /root/.profile
 
 git clone https://github.com/$GITHUB_USERNAME/app-on-openstack.git /root/app-on-openstack
 pip install virtualenv
-virtualenv app-venv
-source app-venv/bin/activate
+virtualenv /root/app-venv
+source /root/app-venv/bin/activate
 pip install -r /root/app-on-openstack/code/app/requirements.txt
 python /root/app-on-openstack/code/app/manage.py runserver --host 0.0.0.0 &
