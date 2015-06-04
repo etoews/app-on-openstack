48515a615778b5cfc9ccfe68932038fae9434e30
Everett Toews <everett.toews@rackspace.com>
2015-06-04 11:42:59 -0500

    Remove haproxy config file because it has too much personality

--- code/lb/deploy.sh
+++ code/lb/deploy.sh
@@ -6,9 +6,15 @@ apt-get -y -qq install haproxy
 IFS=$'\n\t'
 source /root/ips.rc
 
+cat <<EOF >> /etc/haproxy/haproxy.cfg
+
+listen  web-proxy 0.0.0.0:80
+        mode http
+        balance roundrobin
+EOF
+
 for IP in ${WM_IPS[@]}; do
-  echo "        server app $IP" >> /root/lb.haproxy
+  echo "        server app $IP" >> /etc/haproxy/haproxy.cfg
 done
 
-cp /root/lb.haproxy /etc/haproxy/haproxy.cfg
 service haproxy restart
