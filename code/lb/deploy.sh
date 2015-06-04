27a67d64cf8fbd553a9dbca47814eb9834d06897
Everett Toews <everett.toews@rackspace.com>
2015-06-04 00:00:14 -0500

    Remove unnecessary code

--- code/lb/deploy.sh
+++ code/lb/deploy.sh
@@ -12,17 +12,3 @@ done
 
 cp /root/lb.haproxy /etc/haproxy/haproxy.cfg
 service haproxy restart
-
-
-git clone https://github.com/$GITHUB_USERNAME/app-on-openstack.git /root/app-on-openstack
-pip install virtualenv
-virtualenv /root/app-venv
-source /root/app-venv/bin/activate
-pip install -r /root/app-on-openstack/code/app/requirements.txt
-cd /root/app-on-openstack/code/app; gunicorn manage:app -b localhost:8000 &
-
-/etc/init.d/nginx start
-rm /etc/nginx/sites-enabled/default
-cp /root/app-on-openstack/code/app/app.nginx /etc/nginx/sites-available/app
-ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled/app
-/etc/init.d/nginx restart
